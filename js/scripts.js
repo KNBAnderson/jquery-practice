3f4b17eefc7236cb4eca63f4f2d65424f04ce424
Katlin Anderson and Mathew Akre <mathew@>
2019-03-26 11:20:49 -0700

    add background collor and font family for each walrus when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
   $("#toggle-container .clickable").click(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle().hide();
+    $("body").removeClass();
+    $("body").addClass("tan-background")
   });
 
   $("#toggle-container img.clickable").click(function() {
+    $("body").removeClass();
+    $("body").addClass("orange-background");
     $("#walrus-fade-in").fadeIn();
 
   });
 
   $("#fade-container img.clickable").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
     $("#walrus-fade-in").fadeOut();
     $("#walrus-slide-in").slideToggle();
     // $("#walrus-slide-out").slideOut();
